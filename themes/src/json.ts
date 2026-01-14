a511486d97b73ca3778118a6cea86005ed7d3221
Tien Vu <viettienvu.97@gmail.com>
2026-01-14 15:05:03 +0700

    feat: update JSON token color mapping and fix formatting in json.ts

--- themes/src/json.ts
+++ themes/src/json.ts
@@ -4,24 +4,27 @@ import type { TokenColor } from './types'
 
 const jsonScopes = {
   'source.json': {
-    'meta.structure.dictionary.json': {
-      'string.json': {
-        'support.type.property-name.json': {
-          '': palette.violet,
-          'punctuation.support.type.property-name.begin.json': palette.red,
-          'punctuation.support.type.property-name.end.json': palette.red
+    ' meta.structure.dictionary.json': {
+      ' string.json': {
+        ' support.type.property-name.json': {
+          ' ': palette.orange,
+          ' punctuation.support.type.property-name.begin.json': palette.red,
+          ' punctuation.support.type.property-name.end.json': palette.red
         }
       },
-      'meta.structure.dictionary.value.json': {
-        'punctuation.separator.dictionary.key-value.json': palette.leon,
-        'string.quoted.double.json': {
-          '': palette.green,
-          'punctuation.definition.string.begin.json': palette.red,
-          'punctuation.definition.string.end.json': palette.red
-        }
+      ' meta.structure.dictionary.value.json': {
+        ' punctuation.separator.dictionary.key-value.json': palette.leon,
+        ' string.quoted.double.json': {
+          '': palette.leon,
+          ' punctuation.definition.string.begin.json': palette.red,
+          ' punctuation.definition.string.end.json': palette.red
+        },
+        ' constant.language.json': palette.violet,
+        ' constant.numeric.json': palette.green,
+        ' punctuation.separator.dictionary.pair.json': palette.leon
       }
     }
   }
 }
-const jsonTokenColors: TokenColor[] = mapScopesToTokenColors(jsonScopes)
+const jsonTokenColors: TokenColor[] = [...mapScopesToTokenColors(jsonScopes)]
 export default jsonTokenColors
